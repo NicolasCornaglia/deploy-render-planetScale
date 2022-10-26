a572963ba9e429148e96e7a68afd5c55af752b28
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-10-26 13:44:59 -0300

    trying to fix connection issues 6

--- database/config/config.js
+++ database/config/config.js
@@ -16,7 +16,7 @@ module.exports = {
     "database": DB_NAME,
     "host": DB_HOST,
     "port": DB_PORT,
-    "dialect": "mariadb",
+    "dialect": DB_DIALECT,
     "allowPublicKeyRetrieval": true
   },
   "production": {
@@ -25,7 +25,7 @@ module.exports = {
     "database": DB_NAME,
     "host": DB_HOST,
     "port": DB_PORT,
-    "dialect": "mariadb",
+    "dialect": DB_DIALECT,
     "allowPublicKeyRetrieval": true
   }
 }
