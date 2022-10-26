0f24b1d719bb3a7d3e532fec68b80615c27e9d2b
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-10-26 13:12:21 -0300

    trying to fix connection issues

--- database/config/config.js
+++ database/config/config.js
@@ -10,17 +10,19 @@ module.exports = {
     "dialect": "mariadb"
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "port": DB_PORT,
     "dialect": "mariadb"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": DB_USER,
+    "password": DB_PASSWORD,
+    "database": DB_NAME,
+    "host": DB_HOST,
+    "port": DB_PORT,
     "dialect": "mariadb"
   }
 }
