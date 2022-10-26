cd9785903978889bfe2daf924ff59a70d8cc0cb5
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-10-26 14:02:51 -0300

    trying to fix connection issues 8

--- configDB.js
+++ configDB.js
@@ -5,6 +5,6 @@ const DB_USER = process.env.DB_USER || 'root';
 const DB_PASSWORD = process.env.DB_PASSWORD || null;
 const DB_NAME = process.env.DB_NAME || 'deco_hogar';
 const DB_PORT = process.env.DB_PORT || 3306;
-const DB_DIALECT = process.env.DB_DIALECT || "mariadb";
+const DB_DIALECT = process.env.DB_DIALECT || 'mariadb';
 
 module.exports = {PORT, DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT, DB_DIALECT};
