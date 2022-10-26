a3f47ae37b4024b37c9f417e4b3d54d9656a1c92
NicolasCornaglia <nicolascornaglia0@gmail.com>
2022-10-26 18:04:55 -0300

    keep trying

---
+++ db.js
@@ -0,0 +1,17 @@
+// conection to database in railway
+const { createPool } = require("mysql2/promise");
+const {DB_NAME, DB_HOST, DB_PASSWORD, DB_USER, DB_PORT, DB_DIALECT} = require('./configDB');
+
+const pool = createPool({
+   user: DB_USER,
+   password: DB_PASSWORD,
+   host: DB_HOST,
+   port: DB_PORT,
+   database: DB_NAME,
+
+   dialect: DB_DIALECT,
+   allowPublicKeyRetrieval: true
+})
+
+
+module.exports = { pool };
